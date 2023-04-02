878e9395a9985d99ac27d53d612dc1624ad5c21f
shilpasy <shilpa.yadav@metropolia.fi>
2023-04-02 22:21:09 +0300

    initial setup

---
+++ NearYou/NearYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearYouApp.swift
+//  NearYou
+//
+//  Created by Shilpa Singh on 2.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct NearYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
