1251f94953562c31451d30aae5f4ae7e5c8ad02a
Bibek <study.bibekshrestha@gmail.com>
2023-04-05 14:54:31 +0300

    added mapViewModel

---
+++ ViewModel/MapViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  MapViewModel.swift
+//  NearYou
+//
+//  Created by iosdev on 5.4.2023.
+//
+
+import SwiftUI
+import CoreLocationUI
+import MapKit
+
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 60.23683736199204,longitude: 24.82150949060006), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+    
+    let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+    
+    func requestAllowOnceLocationPermission () {
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let latestLocation = locations.first else {
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.region = MKCoordinateRegion(center: latestLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+}
