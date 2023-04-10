c2a4e69f3a229e6e455fbef2a8a258e04b18be16
shilpasy <shilpa.yadav@metropolia.fi>
2023-04-10 14:11:42 +0300

    testing hamburger menu

--- View/HomeView.swift
+++ View/HomeView.swift
@@ -16,70 +16,75 @@ struct HomeView: View {
     @State private var showMenu = false
     @StateObject private var viewModel = MapViewModel()
     @State var searchText = ""
+    @State private var isShowing = true
     
     var body: some View {
         
-        ZStack {
+        NavigationView{
             
-            if selectedTab == 0 {
-                HomeListView()
-            } else {
-                MapView()
-            }
-            
-            VStack{
-                HStack (alignment: .top){
-                    
-                    
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 8)
-                            .fill(Color(.systemGray6))
-                            .frame(width: 30, height: 30)
+            ZStack {
+                
+                if selectedTab == 0 {
+                    HomeListView()
+                } else {
+                    MapView()
+                }
+                
+                VStack{
+                    HStack (alignment: .top){
                         
                         
-                        Button {
-                            //
-                            print("Open the door")
-                        } label: {
-                            Image(systemName: "line.horizontal.3")
-                                .imageScale(.large)
+                        ZStack{
+                            if isShowing {
+                                SideMenuView(isShowing: $isShowing)
+                            }
+                            SideMenuView(isShowing: $isShowing)
+                                .cornerRadius(isShowing ? 20 : 10)
+                                .offset(x: isShowing ? 300 : 0, y: isShowing ? 44 : 0)
+                                .scaleEffect(isShowing ? 0.8 : 1)
+                                .navigationBarItems(leading: Button(action: {
+                                    withAnimation(.spring()) {
+                                        isShowing.toggle()
+                                    }
+                                },label: {
+                                Image(systemName: "line.horizontal.3")
+                                    .imageScale(.large)
+                                    .padding(25)
+                            }))
+                        }
+                        
+                        Spacer()
+                        
+                        
+                        Picker(selection: $selectedTab, label: Text("Select a Tab")){
+                            Text("List").tag(0)
+                            Text("Map").tag(1)
+                        }
+                        .background(Color.white.cornerRadius(10))
+                        .foregroundColor(.blue)
+                        .frame(width: 130)
+                        .padding()
+                        .cornerRadius(20)
+                        .font(.headline)
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                        Spacer()
+                        
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color(.systemGray6))
+                                .frame(width: 30, height: 30)
+                            
+                            Image(systemName: "mappin")
+                                .imageScale(.medium)
                                 .padding(25)
                         }
                     }
-            
                     Spacer()
                     
-                    
-                    Picker(selection: $selectedTab, label: Text("Select a Tab")){
-                        Text("List").tag(0)
-                        Text("Map").tag(1)
-                    }
-                    .background(Color.white.cornerRadius(10))
-                    .foregroundColor(.blue)
-                    .frame(width: 130)
-                    .padding()
-                    .cornerRadius(20)
-                    .font(.headline)
-                    .pickerStyle(SegmentedPickerStyle())
-                    
-                    Spacer()
-                    
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 8)
-                            .fill(Color(.systemGray6))
-                            .frame(width: 30, height: 30)
-                        
-                        Image(systemName: "mappin")
-                            .imageScale(.medium)
-                            .padding(25)
-                    }
                 }
-                Spacer()
-                
             }
         }
-        
-        
     }
 }
 
