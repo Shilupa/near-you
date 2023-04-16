f65b080c81008542b22391b7dfe63a563812c9a4
shilpasy <shilpa.yadav@metropolia.fi>
2023-04-16 18:42:51 +0300

    added new file

---
+++ NearYou/Testview.swift
@@ -0,0 +1,20 @@
+//
+//  Testview.swift
+//  NearYou
+//
+//  Created by Shilpa Singh on 16.4.2023.
+//
+
+import SwiftUI
+
+struct Testview: View {
+    var body: some View {
+        Text("Test View")
+    }
+}
+
+struct Testview_Previews: PreviewProvider {
+    static var previews: some View {
+        Testview()
+    }
+}
